b59e7e1e232ae4fcc5f376a68f4f425be7a75cca
matt <matt@theconstantvariable.com>
2020-07-23 16:00:39 -0700

    Completed coding challenge

--- src/test/scala/simulations/Challenge.scala
+++ src/test/scala/simulations/Challenge.scala
@@ -1,7 +1,12 @@
 package simulations
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
+import scala.util.Random
 
 /**
  * User Journey:
@@ -19,11 +24,33 @@ import io.gatling.http.Predef._
  */
 class Challenge extends Simulation {
 
-  val httpConf = http.baseUrl("https://localhost:8080/app/")
+  val httpConf = http.baseUrl("http://localhost:8080/app/")
     .header("Accept", "application/json")
 
+  var idNumber = 21
+  val random = new Random()
+  val now = LocalDate.now()
+  val pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+  def randomString(length: Int) = {
+    random.alphanumeric.filter(_.isLetter).take(length).mkString
+  }
+
+  def getRandomDate(startDate: LocalDate, random: Random) = {
+    startDate.minusDays(random.nextInt(30)).format(pattern)
+  }
+
+  val feeder = Iterator.continually(Map(
+    "gameId" -> idNumber,
+    "name" -> ("Game-" + randomString(5)),
+    "releaseDate" -> getRandomDate(now, random),
+    "reviewScore" -> random.nextInt(100),
+    "category" -> ("Category-" + randomString(6)),
+    "rating" -> ("Rating-" + randomString(4))
+  ))
+
   def postNewVideoGame() = {
-    feed()
+    feed(feeder)
       .exec(http("Create a video game")
         .post("videogames/")
         .body(ElFileBody("bodies/NewGameTemplate.json")).asJson
@@ -31,22 +58,25 @@ class Challenge extends Simulation {
       .pause(1)
   }
 
-  def getVideoGameByName(name: String) = {
-
+  def getVideoGameById(id: Int) = {
+    exec(http("Get video game by id #")
+    .get("videogames/" + id.toString)
+    .check(status.is(500)))
+      .pause(1)
   }
 
-  def deleteVideoGame(name: String) = {
-
+  def deleteVideoGame(id: Int) = {
+    exec(http("Delete video game by id #")
+    .delete("videogames/" + id.toString)
+    .check(status.is(200)))
+      .pause(1)
   }
 
   val scn = scenario("Create, retrieve, delete and confirm deletion of video game")
     .exec(postNewVideoGame())
-    .pause(5)
-    .exec(getVideoGameByName())
-    .pause(5)
-    .exec(deleteVideoGame())
-    .pause(5)
-    .exec(getVideoGameByName())
+    .exec(getVideoGameById(idNumber))
+    .exec(deleteVideoGame(idNumber))
+    .exec(getVideoGameById(idNumber))
 
   setUp(
     scn.inject(atOnceUsers(1))
